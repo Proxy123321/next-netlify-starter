76289610c2e9b77087b21e328b1c928b360bf86c
Proxy123321 <112669025+Proxy123321@users.noreply.github.com>
2023-01-26 11:13:06 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,11 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Welcome to Poopy Games 3kho" />
+        <p className="This is Poopy Game new Game site">
+          
+        </p> 
+    <p>Click the start button to see all of our wonderful games.</p>
       </main>
 
       <Footer />
